99dfcc61abf3a238321e7eef8ca5248a14e502ba
knowledge35 <ibomyakmc@hotmail.com>
2020-09-05 15:04:12 +0300

    overflowing

---
+++ Overflowing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace overflowing
+{
+    class Program
+    {
+        static void Main()
+        {
+            byte n = 255;
+            int z;
+            //byte z = n + 1; overflowing
+            z = n + 1;
+            Console.WriteLine(z);
+            Console.ReadLine();
+        }
+    }
+}
