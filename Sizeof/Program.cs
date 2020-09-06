cd22afc918a7267f17b03c26a2375e3fb2735e99
knowledge35 <ibomyakmc@hotmail.com>
2020-09-06 14:57:22 +0300

    sizeof

---
+++ Sizeof/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Sizeof
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //you can get the exact size of a type as byte  
+            Console.WriteLine("Size of int: {0}",sizeof(int));
+            Console.ReadLine();
+        }
+    }
+}
